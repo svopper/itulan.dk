13064fc0eb832ba770d473d39cd68e455b7cc65e
Kasper Olsen <root@kols.dk>
2019-08-28 16:30:07 +0200

    now fetching real posts

---
+++ src/components/Post.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="fb-post">
+      <p class="date">{{ date }}</p>
+      <p class="message">{{ message }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["date", "message"]
+};
+</script>
+
+<style scoped>
+.fb-post {
+  margin: 1rem auto;
+  padding: 0.4rem 3rem;
+  border: 4px #2f3eac solid;
+}
+
+.message {
+  font-size: 2.5rem;
+  margin-top: 0px;
+}
+
+.date {
+  /* float: right; */
+}
+</style>
--- src/views/Stream.vue
+++ src/views/Stream.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
     <h1>Stream</h1>
-    <iframe
-      src="https://embed.twitch.tv?channel=itulan&amp;referrer=http%3A%2F%2Fitulan.dk%2Fstream%2F"
-      allowfullscreen
-      scrolling="no"
-      frameborder="0"
-      allow="autoplay; fullscreen"
-      width="100%"
-      height="60%"
-    ></iframe>
+    <!-- <video src="https://player.twitch.tv/?volume=0.5&!muted&channel=beyondthesummit"></video> -->
+    <div class="stream-content">
+      <div class="overla"></div>
+      <iframe
+        class="stream"
+        src="https://player.twitch.tv/?volume=0.5&!muted&channel=beyondthesummit"
+        allowfullscreen
+        scrolling="no"
+        frameborder="0"
+        allow="autoplay; fullscreen"
+        width="100%"
+        height="60%"
+      ></iframe>
+    </div>
   </div>
 </template>
+
+<style>
+.overlay {
+  background: rgba(0, 0, 0, 0.75);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.stream {
+  position: relative;
+}
+</style>
