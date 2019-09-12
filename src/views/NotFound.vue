d51e3c29676c7f2009cc0804f180744b0d7b05d6
Kasper Olsen <root@kols.dk>
2019-09-12 23:05:06 +0200

    fixed 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
+  <div class="not-found">
     <h1>404</h1>
   </div>
 </template>
+
+<style scoped>
+.not-found {
+  margin: 80px auto;
+  max-width: 800px;
+  padding: 15px;
+  text-align: center;
+}
+</style>
