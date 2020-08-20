bd52db9813a975191d448330d3b96e0741861162
Kasper Olsen <root@kols.dk>
2020-08-20 22:48:18 +0200

    cont on joinus

--- src/views/JoinUs.vue
+++ src/views/JoinUs.vue
@@ -9,6 +9,34 @@
       we'd love to welcome you to our crew!
     </p>
 
+    <form onsubmit="event.preventDefault(); validateMyForm();">
+      <label for="fname">First Name</label>
+      <input
+        type="text"
+        id="fname"
+        name="firstname"
+        placeholder="Your name.."
+      />
+
+      <label for="lname">Last Name</label>
+      <input
+        type="text"
+        id="lname"
+        name="lastname"
+        placeholder="Your last name.."
+      />
+
+      <label for="subject">Subject</label>
+      <textarea
+        id="subject"
+        name="subject"
+        placeholder="Write something.."
+        style="height:200px"
+      ></textarea>
+
+      <input type="submit" value="Submit" />
+    </form>
+
     <a
       class="button"
       href="mailto:chairman@itulan.dk?Subject=ITU%20LAN%20Application"
@@ -22,4 +50,43 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  box-sizing: border-box;
+}
+
+input[type="text"],
+select,
+textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type="submit"] {
+  background-color: #4caf50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type="submit"]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
